ac37767fc8ac89a51275ae1a3b0029d784bb1727
Julie Go <julie.gosselin84@yahoo.fr>
2022-07-11 11:56:21 +0200

    Initial Commit

---
+++ NoeudsEtCroix/NoeudsEtCroixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoeudsEtCroixApp.swift
+//  NoeudsEtCroix
+//
+//  Created by User on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NoeudsEtCroixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
